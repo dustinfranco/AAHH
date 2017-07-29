5b83b808a1dcfc064b85596c3e913b615c9e1d4c
Dustin Franco <dustin@edyn.com>
2017-07-28 23:25:23 -0700

    added function to configure all pins as low to prevent literal fires.

--- GPIOSetup.py
+++ GPIOSetup.py
@@ -16,9 +16,22 @@ activePinsMap = [\
 18,26,22,38,40,24,\
 33,36,37,31,35,32,
  0, 0, 0, 0, 0, 0];
-aweg
+
 #Inactive pins:
 inactivePins = [];
 for pin in allValidPins:
 	if(pin not in activePinsMap):
 		print(str(pin) + " is inactive");
+
+#Initialize ALL pins as low out:
+def configureAllPinsLow():
+    print ('Configuring all pins low out');
+    for x in range(0,len(allValidPins)):
+        try:
+            GPIO.setup(allValidPins[x], GPIO.OUT);
+            GPIO.output(allValidPins[x], False);
+        except Exception as e:
+            print (str(e) + ': ' + str(activePins[x]));
+    print ('Finished configuring pins');
+
+configureAllPinsLow();
