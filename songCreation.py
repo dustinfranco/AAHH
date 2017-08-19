a4d4861e08f006805ebe786752f6f874e0e2a16b
Dustin Franco <dustin@edyn.com>
2017-08-19 00:35:10 -0700

    added beginning of song creation file

---
+++ songCreation.py
@@ -0,0 +1,13 @@
+
+
+def compileSong(songName):
+	print ("compile song not complete");
+
+def editSong(songName):
+	print ("editSong not complete");
+
+def playSong(songName):
+	print ("playSong not complete");
+
+def createNewSong(songName):
+	print ("createNewSong not complete");
