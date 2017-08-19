b9e28d270a80de3800cb92693c26ecb1d08abce5
Dustin Franco <dustin@edyn.com>
2017-08-19 01:33:18 -0700

    began the song creator

--- songCreation.py
+++ songCreation.py
@@ -1,4 +1,6 @@
-
+newpath = '/home/pi/Desktop/Songs/' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
 
 def compileSong(songName):
 	print ("compile song not complete");
@@ -10,4 +12,15 @@ def playSong(songName):
 	print ("playSong not complete");
 
 def createNewSong(songName):
+	genericSections = ["a", "b", "c", "d", "e", "f", "g", "h"]
+	genericSongStructure = ["i", "m", "c", "b", "o"]
+	if not os.path.exists(newpath):
+	    os.makedirs(newpath + songName)
+	    for structure in genericSongStructure:
+			for section in genericSections:
+	    		os.makedirs(newpath + songName + "/" + structure + section + ".txt");
+				
+		print "creating new song"
+	else :
+		print "song already exists"
 	print ("createNewSong not complete");
