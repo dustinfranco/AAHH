478242210f1f4f871ad6d40d13fa288b20f83451
Dustin Franco <dustin@edyn.com>
2017-08-19 01:56:17 -0700

    now adds all 6 strings to empty files

--- songCreator.py
+++ songCreator.py
@@ -1,5 +1,6 @@
 import os
-basePath = "/home/pi/Desktop/Songs/"
+#basePath = "/home/pi/Desktop/Songs/"
+basePath = "/Users/dustinfranco/Desktop/Songs/"
 if not os.path.exists(basePath):
     print("creating songs folder");
     os.makedirs(basePath);
@@ -22,15 +23,15 @@ def createNewSong(songName, timeSignature = 4):
                 newFile = basePath + songName + "/" + structure + section
                 x = open(newFile, "w")
                 os.utime(newFile, None)
-                for i in range (0,timeSignature):
-                    for j in range (0,4):
-                        x.write("--");
-                    if(i < timeSignature - 1):
-                        x.write("||");
-                songSequence = basePath + songName + "
-                /" + "songSequence";
+                for m in range(0,6):
+                    for i in range (0,timeSignature):
+                        for j in range (0,4):
+                            x.write("--");
+                        if(i < timeSignature - 1):
+                            x.write("||");
+                    x.write("\n");
+                songSequence = basePath + songName + "/" + "songSequence";
                 x = open(songSequence, "w")
                 os.utime(newFile, None)
                 x.write("ia,ia,ib,ic\nma,mb,mc,md\n")
                
-createNewSong("threeSignature", 3);
