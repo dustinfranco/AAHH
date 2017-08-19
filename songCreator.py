9ab0a5797ecc578e9adcefbafd6b02b2131d5e30
Dustin Franco <dustin@edyn.com>
2017-08-19 01:47:32 -0700

    rewrote songCreator

---
+++ songCreator.py
@@ -0,0 +1,36 @@
+import os
+basePath = "/home/pi/Desktop/Songs/"
+if not os.path.exists(basePath):
+    print("creating songs folder");
+    os.makedirs(basePath);
+def compileSong(songName):
+    print("compile song not complete");
+
+def editSong(songName):
+        print("editSong not comlete");
+
+def playSong(songName):
+    print("playSong not complete");
+
+def createNewSong(songName, timeSignature = 4):
+    genericSections = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    genericSongStructure = ["i", "m", "c", "b", "o"]
+    if(not os.path.exists(basePath + songName)):
+        os.makedirs(basePath + songName);
+        for structure in genericSongStructure:
+            for section in genericSections:
+                newFile = basePath + songName + "/" + structure + section
+                x = open(newFile, "w")
+                os.utime(newFile, None)
+                for i in range (0,timeSignature):
+                    for j in range (0,4):
+                        x.write("--");
+                    if(i < timeSignature - 1):
+                        x.write("||");
+                songSequence = basePath + songName + "
+                /" + "songSequence";
+                x = open(songSequence, "w")
+                os.utime(newFile, None)
+                x.write("ia,ia,ib,ic\nma,mb,mc,md\n")
+               
+createNewSong("threeSignature", 3);
