c8df29d3762ad68480c5752044966e2e53a117dd
Dustin Franco <dustin@edyn.com>
2018-07-21 21:27:34 -0700

    remakes dmxd.c on pi and remakes song locally

--- play_songs/src/play_song_remotely.py
+++ play_songs/src/play_song_remotely.py
@@ -21,35 +21,62 @@ if(argv[1][0] == "h"):
     print "first argument should be song name"
     print "-dm to disable 'remake dmxd.o and scp it'"
     print "-drc to disable recompiling the song"
+    
     print "-dhw to disable hardware optimization"
-    print "-debug to print debug statements in dmxd.o when it runs"
+    print("-debug_song to print debug statements in song creator")
+    print "-debug_dmxd to print debug statements in dmxd.o when it runs"
+    
     print "-bpm (number) to set static bpm"
     print "\n"
     exit()
 
-t=None
-if(True):
-#try:
-    t = paramiko.Transport((hostname, port))
-    t.connect(username=username, password=password)
-    sftp = paramiko.SFTPClient.from_transport(t)
-
-    #all arguments as a single string with space delimiter:
-    args_as_string = " ".join(argv[1:])   
-    #recompile song
-    cmd = "sudo python {}/play_songs/src/songCreator.py {}".format(source, args_as_string)
-    #call(cmd.split())
-    #recompile dmxd.o
-    cmd = "sudo make -C {}/play_songs/src/".format(source)
-    print(cmd)
-    call(cmd.split())
+try:
+    #ssh
+    t = paramiko.client.SSHClient()
+    t.set_missing_host_key_policy(paramiko.client.WarningPolicy)
+    t.connect(
+            #set_missing_host_key_policy=False,
+            hostname=hostname,
+            port=port,
+            username=username,
+            look_for_keys=False,
+            password=password
+        )
+    tsftp=t.open_sftp()
     
-    #sftp.put(source, dest)
-"""
+    args_as_string = " ".join(argv[1:])   
+    
+    #recompile song
+    if "-drc" in argv:
+        print "not recompiling song"
+    else:
+        cmd = "sudo python {}/play_songs/src/songCreator.py {}".format(source, args_as_string)
+        call(cmd.split())
+    
+
+
+    print("")
+
+    #recompile dmxd.o (on pi):
+    if "-dm" in argv:
+        print("not remaking dmxd.o")
+    else:
+        tsftp.put(
+            source + "/play_songs/src/dmxd.c",
+            dest + "/play_songs/src/dmxd.c"
+        )
+        cmd = "make -C {}/play_songs/src/".format(dest) 
+        print(cmd)
+        stdin, stdout, stderr = t.exec_command(cmd)
+        if(stderr):
+            print("STDERR:")
+            print("".join(stderr.readlines()))
+        print("STDOUT:")
+        print("".join(stdout.readlines()))
+        print("finished making dmxd.c successfully")
+
 except Exception as e:
     print(sys.exc_info())
 
 finally:
     t.close()
-"""
-t.close()
